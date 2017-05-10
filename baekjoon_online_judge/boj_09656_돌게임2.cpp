63e08355af22187014aee6489149d36729612e10
seventeen <powdragon1@gmail.com>
2017-05-10 18:34:30 +0900

    boj: [9656]돌 게임2
    Signed-off-by: seventeen <powdragon1@gmail.com>

---
+++ baekjoon_online_judge/boj_09656_돌게임2.cpp
@@ -0,0 +1,19 @@
+/**
+ * About
+ *
+ * Author: seventeeen@GitHub <powdragon1@gmail.com>
+ * Date  : 2017-05-10
+ * URL   : https://www.acmicpc.net/problem/9656
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int x;
+    cin >> x;
+    cout << (x&1 ? "CY" : "SK");
+}
